38be54f4f48143ea165fafb8241b6f37e6ad0e47
Selcuk Buyuktanir <selcuk.buyuktanir@gmail.com>
2022-10-16 21:44:05 +0200

    UML file is included

---
+++ src/main/java/at/spengergasse/friseursalon/FriseurSalonApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.friseursalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriseurSalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriseurSalonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/friseursalon/FriseurSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.friseursalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriseurSalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
